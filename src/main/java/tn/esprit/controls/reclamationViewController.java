981c2e0199189b0024d64940a74baf0c74026774
AMG9980 <amine.gharbi.1@esprit.tn>
2024-02-20 14:42:33 +0100

    Second Version

---
+++ src/main/java/tn/esprit/controls/reclamationViewController.java
@@ -0,0 +1,18 @@
+package tn.esprit.controls;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class reclamationViewController implements javafx.fxml.Initializable{
+    public reclamationViewController() { /* compiled code */ }
+
+    public void initialize(java.net.URL url, java.util.ResourceBundle rb) { /* compiled code */ }
+
+    @javafx.fxml.FXML
+    private void close(javafx.scene.input.MouseEvent event) { /* compiled code */ }
+
+    @javafx.fxml.FXML
+    private void getAddView(javafx.scene.input.MouseEvent event) { /* compiled code */ }
+
+
+}
--- src/main/java/tn/esprit/test/Main.java
+++ src/main/java/tn/esprit/test/Main.java
@@ -1,7 +1,23 @@
 package tn.esprit.test;
 
+import tn.esprit.models.Reclamation;
+import tn.esprit.services.serviceReclamation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Reclamation p1 = new Reclamation(1000, 1, 101, 201, 301, "Open", "Description 1", createDate("2022-02-16"), createDate("2022-02-16"));
+        Reclamation p2 = new Reclamation(8945, 2, 102, 202, 302, "In Progress", "Description 2", createDate("2022-02-16"), createDate("2022-02-16"));
+        Reclamation p3 = new Reclamation(4565, 3, 103, 203, 303, "Closed", "Description 3", createDate("2022-02-16"), createDate("2022-02-16"));
+
+        serviceReclamation sp = new serviceReclamation();
+        sp.add(p1);
+        sp.add(p2);
+        sp.add(p3);
+
+        System.out.println(sp.getAll());
     }
 }

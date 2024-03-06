fe3e1f6fb9c598a12b5849253c54670f10722098
oussamahajjem <oussema.hajjem@esprit.tn>
2024-03-06 02:29:41 +0100

    16commit

--- src/main/java/controllers/AfficherProjetController.java
+++ src/main/java/controllers/AfficherProjetController.java
@@ -73,6 +73,80 @@ public class AfficherProjetController implements Initializable {
             }
         });
     }
+    @FXML
+    void sortDate(ActionEvent event) {
+        // Tri de pnItems par date de projet
+        ObservableList<Node> nodes1 = FXCollections.observableArrayList(pnItems1.getChildren());
+        Collections.sort(nodes1, new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                String date1 = ((Label) node1.lookup("#dateD")).getText();
+                String date2 = ((Label) node2.lookup("#dateD")).getText();
+                return date1.compareTo(date2);
+            }
+        });
+        pnItems1.getChildren().setAll(nodes1);
+
+        // Tri de pnItems1 par date de collaboration
+        ObservableList<Node> nodes2 = FXCollections.observableArrayList(pnItems.getChildren());
+        Collections.sort(nodes2, new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                String date1 = ((Label) node1.lookup("#date")).getText();
+                String date2 = ((Label) node2.lookup("#date")).getText();
+                return date1.compareTo(date2);
+            }
+        });
+        pnItems.getChildren().setAll(nodes2);
+    }
+
+    @FXML
+    void sortType(ActionEvent event) {
+        // Tri de pnItems par typeColl
+        ObservableList<Node> nodes2 = FXCollections.observableArrayList(pnItems.getChildren());
+        Collections.sort(nodes2, new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                String typeColl1 = ((Label) node1.lookup("#type")).getText();
+                String typeColl2 = ((Label) node2.lookup("#type")).getText();
+                return typeColl1.compareToIgnoreCase(typeColl2);
+            }
+        });
+        pnItems.getChildren().setAll(nodes2);
+
+    }
+    @FXML
+    void sortP(ActionEvent event) {
+        // Tri de pnItems1 par nomPr
+        ObservableList<Node> nodes1 = FXCollections.observableArrayList(pnItems1.getChildren());
+        Collections.sort(nodes1, new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                String nomPr1 = ((Label) node1.lookup("#nomPr")).getText();
+                String nomPr2 = ((Label) node2.lookup("#nomPr")).getText();
+                return nomPr1.compareToIgnoreCase(nomPr2);
+            }
+        });
+        pnItems1.getChildren().setAll(nodes1);
+
+
+    }
+
+    @FXML
+    void sortCA(ActionEvent event) {
+        ObservableList<Node> nodes1 = FXCollections.observableArrayList(pnItems1.getChildren());
+        Collections.sort(nodes1, new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                Double CA1 = Double.parseDouble(((Label) node1.lookup("#CA")).getText());
+                Double CA2 = Double.parseDouble(((Label) node2.lookup("#CA")).getText());
+                return CA1.compareTo(CA2);
+            }
+        });
+        pnItems1.getChildren().setAll(nodes1);
+
+    }
+
 
     private void handleSearch(String searchText) throws IOException {
         searchText = searchText.toLowerCase(); // Convertir le texte de recherche en minuscules
@@ -82,7 +156,7 @@ public class AfficherProjetController implements Initializable {
 
         // Parcourir tous les projets
         for (Projet projet : projets) {
-            if (projet.getNomPr().toLowerCase().contains(searchText)) {
+            if (projet.getNomPr().toLowerCase().contains(searchText)||String.valueOf(projet.getCA()).toLowerCase().contains(searchText)) {
                 // Créer un VBox représentant le projet et l'ajouter à pnItems1
                 VBox projetNode = createProjetNode(projet);
                 pnItems1.getChildren().add(projetNode);
@@ -170,6 +244,15 @@ public class AfficherProjetController implements Initializable {
 
         return vbox;
     }
+    private String getNomProjetFromId(int idProjet) {
+        for (Projet projet : projets) {
+            if (projet.getId() == idProjet) {
+                return projet.getNomPr();
+            }
+        }
+        return ""; // Retourne une chaîne vide si aucun projet correspondant n'est trouvé
+    }
+
 
 
     @FXML
@@ -180,10 +263,16 @@ public class AfficherProjetController implements Initializable {
             // Récupérer les données pour le graphique
             HashMap<Integer, Double> data = statistiqueCollaboration.generateData();
 
+            // Calculer la somme totale des valeurs pour obtenir les pourcentages
+            double total = data.values().stream().mapToDouble(Double::doubleValue).sum();
+
             // Convertir les données en une liste observable de PieChart.Data
             ObservableList<PieChart.Data> dataList = FXCollections.observableArrayList();
             for (Integer idProjet : data.keySet()) {
-                dataList.add(new PieChart.Data("Projet ID " + idProjet, data.get(idProjet)));
+                String nomProjet = getNomProjetFromId(idProjet);
+                double valeur = data.get(idProjet);
+                double pourcentage = (valeur / total) * 100;
+                dataList.add(new PieChart.Data(nomProjet + " (" + String.format("%.2f", pourcentage) + "%)", valeur));
             }
 
             // Charger le fichier FXML de la vue de statistiques
@@ -204,6 +293,8 @@ public class AfficherProjetController implements Initializable {
             e.printStackTrace();
         }
     }
+
+
 
     @FXML
     void actualiser(ActionEvent event) {
